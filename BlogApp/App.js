5d6a5d1dab15ac5863bcbc2271ed6ab5d05a4e3a
Tahmid <tahmidislam1998@gmail.com>
2020-12-14 04:05:35 +0600

    existing project

---
+++ BlogApp/App.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import {NavigationContainer, navigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import {  AntDesign ,Ionicons ,Entypo } from "@expo/vector-icons";
+import { DefaultTheme, DarkTheme } from '@react-navigation/native';
+import SignInScreenActivity from './Source/screens/SignInScreen'
+import SignUpScreenActivity from './Source/screens/SignUpScreen'
+import HomeScreenActivity from './Source/screens/Home'
+import ProfileScreenActivity from './Source/screens/ProfileScreen'
+import NotificationScreenActivity from './Source/screens/NotificationScreen'
+import IndividualPostScreen from './Source/screens/IndividualPost'
+import { AuthContext, AuthProvider } from "./Source/provider/AuthProvider";
+
+const AuthStack= createStackNavigator();
+const HomeStack =createStackNavigator();
+const HomeTab = createMaterialBottomTabNavigator();
+const AppDrawer = createDrawerNavigator();
+
+const AppDrawerScreen = () => {
+  return (
+    <AppDrawer.Navigator theme={MyTheme} initialRouteName="Home">
+      <AppDrawer.Screen name="Home" component={HomeTabScreen} />
+      <AppDrawer.Screen name="Profile" component={ProfileScreenActivity} />
+    </AppDrawer.Navigator>
+  );
+};
+
+const HomeTabScreen = () => {
+  return (
+    <AuthContext.Consumer>
+        {(auth) => (
+    <HomeTab.Navigator initialRouteName="Home">
+      <HomeTab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" color="white" size={26} />
+            ) : (
+              <AntDesign name="home" color="white" size={22} />
+            ),
+        }}
+      />
+      <HomeTab.Screen
+        name="Notification"
+        children={()=><NotificationScreenActivity currentUser={auth.CurrentUser} />}
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="ios-notifications" size={26} color="white" />
+            ) : (
+              <Ionicons
+                name="ios-notifications-outline"
+                size={22}
+                color="white"
+              />
+            ),
+        }}
+      />
+    </HomeTab.Navigator>
+     )}
+     </AuthContext.Consumer>
+  );
+};
+
+
+const HomeStackScreen=() =>{
+  return(
+    <HomeStack.Navigator initialRouteName="Home">
+      <HomeStack.Screen name="Home" component={HomeScreenActivity}  options={{ headerShown: false }}/>
+      <HomeStack.Screen name="IndivialPost" component={IndividualPostScreen}  options={{ headerShown: false }}/>
+    </HomeStack.Navigator>
+  )
+}
+
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator initialRouteName="SignIn">
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignInScreenActivity}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="SignUp"
+        component={SignUpScreenActivity}
+        options={{ headerShown: false }}
+      />
+    </AuthStack.Navigator>
+  );
+};
+
+const MyTheme ={
+  dark:true,
+  colors:{
+    primary: "#ffffff",
+    background: "#17001a",
+    card: "#17001a",
+    text: "#ffffff",
+    border: "#000028",
+    notification:"#9933FF",
+  
+    
+  },
+};
+
+export default function App() {
+
+  
+  
+  return (
+
+    <AuthProvider>
+      <AuthContext.Consumer>
+        {(auth) => (
+          <NavigationContainer theme={MyTheme}>
+             {auth.IsLoggedIn ? <AppDrawerScreen /> : <AuthStackScreen />}
+          </NavigationContainer>
+        )}
+      </AuthContext.Consumer>
+    </AuthProvider>
+    
+    
+   
+  );
+}
---
+++ BlogApp/Source/shareable/PostList.js
@@ -0,0 +1,190 @@
+import React , { useState, useEffect }  from 'react'
+import {View,Button,Flatlist,Text,ActivityIndicator,StyleSheet,TouchableOpacity} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {PostCard} from '../shareable/customCard'
+import { getDataJSON, storeDataJSON } from "../Function/AsyncStorageFunction";
+import { Zocial } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+
+const PostList =(props)=>{
+   
+    const posts=props.posts
+    const nav=props.nav
+    const currUser=props.currentUser
+  
+
+    const [Name, setName] = useState("");
+    const [iconName,setIconName]=useState("hearto")
+    const [loading, setLoading] = useState(false);
+    const [likeCount,setLikeCount]=useState(0)
+    const [commentCount,setCommentCount]=useState(0)
+    const [comments,setComments]= useState([]);
+    
+   
+    const [authorPostReactions, setAuthorPostReactions] = useState([]);
+    const [liker, setLikers] = useState([]);
+
+
+    const FindUser=async()=>{
+     
+        let response= await getDataJSON(posts.Email)
+        let postReaction=await getDataJSON(posts.Email+"Reaction")
+        let postLiker=await getDataJSON(posts.key+"likes")
+        let postComments=await getDataJSON(posts.key+"Comment")
+        if(postComments.length>0){
+           // console.log("Key"+posts.key)
+       // console.log(postComments.length)
+                setComments(postComments)
+                setCommentCount(postComments.length)
+        }
+
+        if(postLiker.length>0){
+        setLikeCount(postLiker.length)
+        setLikers(postLiker)
+        setAuthorPostReactions(postReaction)
+        }
+     
+       
+        setName(response.name)
+        setLoading(true)
+ 
+    }
+
+
+    if(!loading){
+    FindUser()
+    }
+    
+    
+    //console.log({Name})
+    if(loading){
+    return(
+       
+       <PostCard>
+           <Zocial name="statusnet" size={24} color="#fc6a03"  style={styles.iconStyle} />
+           <Text style={styles.authorNameStyle}>{Name}</Text>
+           <Text style={styles.dateStyle}>{posts.postDate}</Text>
+           <Text style={styles.postBodyStyle}>{posts.postText}</Text>
+          
+           <FontAwesome name="comment-o" size={27} color="#fc6a03"  style={styles.commentStyle}
+           onPress={function(){
+            nav.navigation.navigate("IndivialPost",  {posts,Name,comments,likeCount,commentCount,authorPostReactions,currUser} );
+           
+            console.log("commento")
+            console.log(likeCount+" pera nai"+ commentCount)
+           }}/>
+
+          
+           <AntDesign name={iconName} size={24} color="#fc4601"  style={styles.likeStyle} 
+           onPress ={function(){
+            setIconName("heart")
+            //console.log(authorPostReactions.length+" length")
+           // console.log(liker)
+            let a=likeCount+1
+            let authorPostCurrentReaction={postId:posts.key,reactor:currUser,status:"like"}
+            authorPostReactions.push(authorPostCurrentReaction)
+            storeDataJSON(posts.Email+"Reaction",authorPostReactions)
+            liker.push(currUser)
+            setLikeCount(a)
+            storeDataJSON(posts.key+"likes",liker)
+           
+            
+            
+           }}/>
+          
+        <Text style={styles.likeTextStyle} >{likeCount} Likes</Text>
+        <Text style={styles.commentTextStyle}>{commentCount} Comments</Text>
+          
+   
+           
+
+       </PostCard>
+    )
+    }
+    else{
+        return(
+            <View style={{ flex: 1, justifyContent: "center" }}>
+            <ActivityIndicator size="large" color="red" animating={true} />
+          </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    iconStyle:{
+        
+        position:'absolute',
+        right:10,
+        top:10,
+       
+        
+        
+       
+
+        
+    },
+    commentStyle:{
+        position:'absolute',
+        bottom:1,
+        right:10,
+        marginBottom:0,
+        
+    },
+    authorNameStyle:{
+        fontFamily:'serif',
+        fontSize:18,
+        color:"#fc6a03",
+        marginBottom:5,
+    },
+    dateStyle:{
+        
+        marginBottom:5,
+        color:"#fc6a03",
+        fontSize:10,
+        fontStyle:"italic"
+    },
+    postBodyStyle:{
+        fontFamily:'serif',
+        marginBottom:10,
+        color:"#fc6a03",
+        fontSize:15,
+        
+    },
+    likeStyle:{
+        marginBottom:3,
+        bottom:0,
+        width:36,
+        left:0
+    },
+   
+    likeTextStyle:{
+        marginBottom:3,
+        fontSize:14,
+        fontFamily:'serif',
+        color:"#fc6a03",
+        
+        width:60,
+        left:30,
+        position:"absolute",
+        bottom:0
+    },
+    commentTextStyle:{
+        marginBottom:3,
+        fontSize:14,
+        fontFamily:'serif',
+        color:"#fc6a03",
+        
+        width:90,
+        right:36,
+        position:"absolute",
+        bottom:0
+    },
+
+  
+}
+);
+
+export default PostList
